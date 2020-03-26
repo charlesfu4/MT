d6a92aa4ac9ac5dcbf92aca0cce7e2bbb1b5d9f0
charles <ccfu@kth.se>
2020-03-26 16:54:28 +0100

    small batch of data for TPOT

--- TPOT/tpot_mulr.py
+++ TPOT/tpot_mulr.py
@@ -5,116 +5,13 @@ import numpy as np
 mul_reg_config_dict = {
     
     #regressors need to be wrapped by multioutput
-    'sklearn.multioutput.MultiOutputRegressor': {
-        'estimator': {
-            'sklearn.ensemble.GradientBoostingRegressor': {
-                'n_estimators': [100],
-                'loss': ["ls", "lad", "huber", "quantile"],
-                'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
-                'max_depth': range(1, 11),
-                'min_samples_split': range(2, 21),  
-                'min_samples_leaf': range(1, 21),
-                'subsample': np.arange(0.05, 1.01, 0.05),
-                'max_features': np.arange(0.05, 1.01, 0.05),
-                'alpha': [0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
-            }
-        }
-    },
-
-    'sklearn.multioutput.MultiOutputRegressor': {
-        'estimator': {
-            'xgboost.XGBRegressor': {
-                'n_estimators': [100],
-                'max_depth': range(1, 11),
-                'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
-                'subsample': np.arange(0.05, 1.01, 0.05),
-                'min_child_weight': range(1, 21),
-                'nthread': [1],
-                'objective': ['reg:squarederror']
-            }    
-        
-        }
-    },
-
-    'sklearn.multioutput.MultiOutputRegressor': {
-        'estimator': {
-            'sklearn.ensemble.AdaBoostRegressor': {
-                'n_estimators': [100],
-                'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
-                'loss': ["linear", "square", "exponential"]
-            }    
-
-        }
-    },
-    'sklearn.multioutput.MultiOutputRegressor': {
-        'estimator': {
-            'sklearn.neighbors.KNeighborsRegressor': {
-                'n_neighbors': range(1, 101),
-                'weights': ["uniform", "distance"],
-                'p': [1, 2]
-            }
-
-        }
-    },
-    'sklearn.multioutput.MultiOutputRegressor': {
-        'estimator': {
-            'sklearn.svm.LinearSVR': {
-                'loss': ["epsilon_insensitive", "squared_epsilon_insensitive"],
-                'dual': [True, False],
-                'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
-                'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5., 10., 15., 20., 25.],
-                'epsilon': [1e-4, 1e-3, 1e-2, 1e-1, 1.]
-            }
-
-        }
-    },
-    'sklearn.multioutput.MultiOutputRegressor': {
-        'estimator': {
-            'sklearn.linear_model.SGDRegressor': {
-                'loss': ['squared_loss', 'huber', 'epsilon_insensitive'],
-                'penalty': ['elasticnet'],
-                'alpha': [0.0, 0.01, 0.001] ,
-                'learning_rate': ['invscaling', 'constant'] ,
-                'fit_intercept': [True, False],
-                'l1_ratio': [0.25, 0.0, 1.0, 0.75, 0.5],
-                'eta0': [0.1, 1.0, 0.01],
-                'power_t': [0.5, 0.0, 1.0, 0.1, 100.0, 10.0, 50.0]
-             }
-         }   
-    },
-
-    #regressors need not wrappers
-    'sklearn.linear_model.MultiTaskElasticNetCV': {
-        'l1_ratio': np.arange(0.0, 1.01, 0.05),
-        'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
-    },
-
-
-    'sklearn.ensemble.ExtraTreesRegressor': {
-        'n_estimators': [100],
-        'max_features': np.arange(0.05, 1.01, 0.05),
-        'min_samples_split': range(2, 21),
-        'min_samples_leaf': range(1, 21),
-        'bootstrap': [True, False]
-    },
-
-
-    'sklearn.tree.DecisionTreeRegressor': {
-        'max_depth': range(1, 11),
-        'min_samples_split': range(2, 21),
-        'min_samples_leaf': range(1, 21)
-    },
 
     'sklearn.ensemble.RandomForestRegressor': {
         'n_estimators': [100],
-        'max_features': np.arange(0.05, 1.01, 0.05),
+        'max_features': np.arange(0.03, 1.01, 0.03),
         'min_samples_split': range(2, 21),
         'min_samples_leaf': range(1, 21),
         'bootstrap': [True, False]
-    },
-
-    'sklearn.linear_model.RidgeCV': {
-       
     },
 
 
