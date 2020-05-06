9a0e85199d23826917e4deb4bfc00ab04ae90079
Chu-Cheng Fu <ccfu@kth.se>
2020-05-06 23:24:27 +0200

    functions fixed

--- manual/functions.py
+++ manual/functions.py
@@ -25,16 +25,18 @@ def quantile_trans(df, n):
     return df_trans
 
 ## confidence interval plot of n's prediction
-def plot_conf(test_y, ypred_t, n, ci_term):
+def plot_conf_dynamic(predicted_error, test_y, ypred_t, n, ci_term):
     # confidence interval
     if(ci_term == 1.96):
         alpha = 5
     elif(ci_term == 1.645):
         alpha = 10
+    elif(ci_term == 1.28):
+        alpha = 20
 
-    mean, err = ci_construct((ypred_t - test_y), ci_term)
-    ypred_t_ub = ypred_t + err + mean
-    ypred_t_lb = ypred_t - err +mean
+    mean, std = ci_construct(predicted_error, ci_term)
+    ypred_t_ub = ypred_t + mean + std 
+    ypred_t_lb = ypred_t + mean - std 
     # plot
     fig = plt.figure(figsize=(16,7))
     font = {'family' : 'Lucida Grande',
@@ -54,51 +56,53 @@ def plot_conf(test_y, ypred_t, n, ci_term):
     plt.legend(loc='upper left', fontsize = 15)
     plt.show()
 
-## convert one to multiple series
-def lag_ahead_series(data, n_in=1, n_out=1, n_vars = 1,dropnan=True):
-    df = pd.DataFrame(data)
-    cols, names = list(), list()
-    # input sequence (t-n, ... t-1)
-    for j in range(n_vars):
-        for i in range(n_in, 0, -1):
-            cols.append(df.iloc[:,j].shift(i))
-            names.append('var{}(t-{})'.format(j+1, i))
-    
-    # forecast sequence (t+1, ... t+n)
-    for j in range(n_vars):
-        for i in range(0, n_out):
-            cols.append(df.iloc[:,j].shift(-i))
-            names += [('var{}(t+{})'.format(j+1, i)) ]
-    # put it all together
-    agg = pd.concat(cols, axis=1)
-    agg.columns = names
-    
-    #drop rows with NaN values
-    if dropnan:
-        agg.dropna(inplace=True)
-    return agg
+def plot_conf_static(test_y, ypred_t, n, ci_term):
+    # confidence interval
+    if(ci_term == 1.96):
+        alpha = 5
+    elif(ci_term == 1.645):
+        alpha = 10
+    elif(ci_term == 1.28):
+        alpha = 20
+        
+    mean, std = ci_construct((test_y.to_numpy() - ypred_t), ci_term)
+    ypred_t_ub = ypred_t + mean + std 
+    ypred_t_lb = ypred_t + mean - std 
+    # plot
+    fig = plt.figure(figsize=(16,7))
+    font = {'family' : 'Lucida Grande',
+            'weight' : 'bold',
+            'size'   : 15}
+    plt.rc('font', **font)
+    plt.style.use('seaborn')
+    plt.plot(ypred_t[n, :].reshape(-1,1), 'gx-',label='Prediction')
+    plt.plot(ypred_t_ub[n, :].reshape(-1,1), 'g--', label='{} % upper bond'.format(100-alpha*0.5))
+    plt.plot(ypred_t_lb[n, :].reshape(-1,1), 'g--', label='{} % lower bond'.format(alpha*0.5))
+    plt.plot(test_y.iloc[n, :].to_numpy().reshape(-1,1), 'ro', label='Ground truth')
+    #plt.fill(np.concatenate([xx, xx[::-1]]),
+    #         np.concatenate([y_upper, y_lower[::-1]]),
+    #         alpha=.5, fc='b', ec='None', label='90% prediction interval')
+    plt.xlabel('hours', **font)
+    plt.ylabel('KWh', **font)
+    plt.legend(loc='upper left', fontsize = 15)
+    plt.show()
 
 ## Distribution plot funciton
 def distri_plot(df):
-    f, axes = plt.subplots(3, 3, figsize=(15, 11), sharex=False)
+    num_cols = df.shape[1]
+    f, axes = plt.subplots(num_cols//3 + 1, 3, figsize=(15, 11), sharex=False)
     for idx, col_name in enumerate(df.columns, 0): 
         idx = int(idx)
-        ## jump to plotting energy
-        if(col_name == "LCLid"):
-            sns.distplot(df["energy"],ax=axes[2,2])
-            return
         sns.distplot(df[col_name],ax=axes[idx//3,idx%3])  
     ## plot     
     plt.tight_layout()
 
 ## Scatter plot function
 def scatter_plot(df):
-    f, axes = plt.subplots(4, 2, figsize=(15, 11), sharex=False)
+    num_cols = df.shape[1]
+    f, axes = plt.subplots(num_cols//2 + 1, 2, figsize=(15, 11), sharex=False)
     for idx, col_name in enumerate(df.columns, 0): 
         idx = int(idx)
-        if(idx >= 8):
-            return
-        ## jump to plotting energy
         sns.scatterplot(x= col_name,y = "energy", data = df, ax=axes[idx//2,idx%2])  
     ## plot     
     plt.tight_layout()
@@ -169,7 +173,7 @@ def extract_dmhq(df):
     return df
 
 ## feature/ target construction fucntion with lag variable
-def feature_target_construct(df, load_lag, target_ahead, temp_lag, f_picked, tskip = 0, wd_on = False, d_on = False, m_on = False, h_on = False, q_on = False):
+def feature_target_construct(df, load_lag, target_ahead, temp_lag, temp_ahead, f_picked, tskip = 0, wd_on = False, d_on = False, m_on = False, h_on = False, q_on = False):
     tempcols = f_picked 
     load = df['energy']
     f_temp = pd.DataFrame()
@@ -179,14 +183,14 @@ def feature_target_construct(df, load_lag, target_ahead, temp_lag, f_picked, tsk
         if(tskip != 0):
             temp = lag_ahead_series(df[col], 
                                  n_in = temp_lag,
-                                 n_out = 0,
+                                 n_out = temp_ahead,
                                  n_vars = 1,
                                  dropnan = True,
                                  nskip= tskip)
         else:
              temp = lag_ahead_series(df[col], 
                                  n_in = temp_lag,
-                                 n_out = 0,
+                                 n_out = temp_ahead,
                                  n_vars = 1,
                                  dropnan = True)
         f_temp = pd.concat([f_temp, temp], axis = 1)            
@@ -212,21 +216,11 @@ def feature_target_construct(df, load_lag, target_ahead, temp_lag, f_picked, tsk
         # alignment
         hour , f = hour.align(f, 'inner', axis = 0)
         f = pd.concat([hour, f], axis = 1)
-    else:
-        hour = df['hour']
-        # alignment
-        hour , f = hour.align(f, 'inner', axis = 0)
-        f = pd.concat([hour, f], axis = 1)
 
     ## quarter one hot on
     if q_on:
         # month one hot encoding
         minute = pd.get_dummies(df['minute'])
-        # alignment
-        minute , f = minute.align(f, 'inner', axis = 0)
-        f = pd.concat([minute, f], axis = 1)
-    else:
-        minute = df['minute']
         # alignment
         minute , f = minute.align(f, 'inner', axis = 0)
         f = pd.concat([minute, f], axis = 1)
@@ -238,11 +232,6 @@ def feature_target_construct(df, load_lag, target_ahead, temp_lag, f_picked, tsk
         # alignment
         weekday , f = weekday.align(f, 'inner', axis = 0)
         f = pd.concat([weekday, f], axis = 1)
-    else:
-        weekday = df['wd']
-        # alignment
-        weekday , f = weekday.align(f, 'inner', axis = 0)
-        f = pd.concat([weekday, f], axis = 1)
         
     ## day one hot on
     if d_on:
@@ -251,21 +240,11 @@ def feature_target_construct(df, load_lag, target_ahead, temp_lag, f_picked, tsk
         # alignment
         day , f = day.align(f, 'inner', axis = 0)
         f = pd.concat([day, f], axis = 1)
-    else:
-        day = df['day']
-        # alignment
-        day , f = day.align(f, 'inner', axis = 0)
-        f = pd.concat([day, f], axis = 1)
     
     ## month one hot on
     if m_on:
         # month one hot encoding
         month = pd.get_dummies(df['month'])
-        # alignment
-        month , f = month.align(f, 'inner', axis = 0)
-        f = pd.concat([month, f], axis = 1)
-    else:
-        month = df['month']
         # alignment
         month , f = month.align(f, 'inner', axis = 0)
         f = pd.concat([month, f], axis = 1)
@@ -291,9 +270,9 @@ def feature_target_construct(df, load_lag, target_ahead, temp_lag, f_picked, tsk
     return f, t
 ## ci construction for err
 def ci_construct(error, n):
-    mean_list = []
     std_list = []
+    mean_list = []
     for i in range(error.shape[1]):
-        mean_list.append(np.mean(error.iloc[:,i]))
-        std_list.append(np.std(error.iloc[:,i])*n)
-    return mean_list, std_list
+        std_list.append(np.std(error[:, i])*n)
+        mean_list.append(np.mean(error[:, i]))
+    return  mean_list, std_list
